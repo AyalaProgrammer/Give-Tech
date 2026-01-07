4bf3ba3187fdf4c8526864c6877060c5500db3ea
ruth-fonfeder <rut7723@gmail.com>
2026-01-07 12:23:33 +0200

    better

--- client/client/vite.config.js
+++ client/client/vite.config.js
@@ -5,3 +5,21 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     // הגדרת Headers כדי לאפשר לחלון ההתחברות של גוגל לתקשר עם האפליקציה
+//     headers: {
+//       "Cross-Origin-Opener-Policy": "same-origin-allow-popups",
+//     },
+//     // ודואים שהשרת רץ על הפורט המקובל (אופציונלי)
+//     port: 5173,
+//   },
+// })
--- server/server.js
+++ server/server.js
@@ -8,22 +8,30 @@ const { Server } = require('socket.io');
 dotenv.config();
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, {
-    cors: { origin: "http://localhost:5173", methods: ["GET", "POST", "PUT"] }
-});
 
+const io = new Server(server, {
+    cors: {
+      origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:5175"],
+      methods: ["GET", "POST"]
+    }
+  });
 app.use(cors());
 app.use(express.json());
 
-// חיבור למסד הנתונים
+
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('✅ מחובר ל-MongoDB Atlas'))
     .catch(err => console.error('❌ שגיאה בחיבור:', err));
 
-// --- חיבור הראוטים החדשים ---
-const volunteerRoutes = require('./routes/volunteerRoutes');
-const messageRoutes = require('./routes/messageRoutes')(io); // מעבירים את io כאן
+const path = require('path');
 
+const volunteerPath = path.join(__dirname, 'routes', 'volunteerRoutes.js');
+const messagePath = path.join(__dirname, 'routes', 'messageRoutes.js');
+
+console.log("Attempting to load routes from:", volunteerPath);
+
+const volunteerRoutes = require(volunteerPath);
+const messageRoutes = require(messagePath)(io);
 app.use('/api/volunteers', volunteerRoutes);
 app.use('/api/messages', messageRoutes);
 
